c4b91346580cf989d661646b4d687e8160e65682
saraivais <isadorasaraiva95@gmail.com>
2022-10-25 15:20:14 -0300

    feat [database]: creates migration for content table~

---
+++ app/backend/src/database/migrations/20221025175338-create-content.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('content', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      body: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('content');
+  },
+};
