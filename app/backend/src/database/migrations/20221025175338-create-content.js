adcd0243afba6b0a0932c19a4dec9a9a031d0d10
saraivais <isadorasaraiva95@gmail.com>
2022-10-25 18:30:41 -0300

    feat [database]: adds user association to content~

--- app/backend/src/database/migrations/20221025175338-create-content.js
+++ app/backend/src/database/migrations/20221025175338-create-content.js
@@ -3,12 +3,21 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('content', {
+    await queryInterface.createTable('contents', {
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
         autoIncrement: true,
         allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id',
+        },
       },
       title: {
         type: Sequelize.STRING,
@@ -18,10 +27,20 @@ module.exports = {
         type: Sequelize.TEXT,
         allowNull: false,
       },
+      created: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: new Date(),
+      },
+      updated: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: new Date(),
+      },
     });
   },
 
   async down(queryInterface) {
-    await queryInterface.dropTable('content');
+    await queryInterface.dropTable('contents');
   },
 };
