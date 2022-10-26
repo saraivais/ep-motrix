643b0f796f0899414a5f836d9340c2c6a2ee4d90
saraivais <isadorasaraiva95@gmail.com>
2022-10-25 21:50:52 -0300

    feat [app]: adds user route, error handler & json parser~

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import userRoute from './routes/user.route';
+import errorMiddleware from './errors/errorMiddleware';
+
+import 'express-async-errors';
 
 const app = express();
 
-app.get('/ping', () => console.log('PONG!'));
+app.use(express.json());
+
+app.use('/users', userRoute);
+
+app.use(errorMiddleware);
 
 export default app;
