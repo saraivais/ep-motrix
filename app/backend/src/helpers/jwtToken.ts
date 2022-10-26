514a03321a2bb3d4cbe99d0e151fbe85752442a5
saraivais <isadorasaraiva95@gmail.com>
2022-10-25 21:39:36 -0300

    feat [helpers]: adds function to generate jsonwebtoken~

--- app/backend/src/helpers/jwtToken.ts
+++ app/backend/src/helpers/jwtToken.ts
@@ -1,1 +1,18 @@
+import 'dotenv/config';
+import * as jwt from 'jsonwebtoken';
+import { SignOptions } from 'jsonwebtoken';
+import IUser from '../interfaces/IUser';
+import CustomError from '../errors/customError';
+import errorCatalog from '../errors/errorCatalog';
+
+const jwtSecret = process.env.JWT_SECRET || 'jwt_secret';
+
+export function generateJwtToken({ id }: IUser):string {
+  const jwtConfig: SignOptions = {
+    algorithm: 'HS256',
+  };
+
+  const token:string = jwt.sign({ data: { id } }, jwtSecret, jwtConfig);
+  return token;
+}
 
