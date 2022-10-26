daac022d1c91e68e56b396edb71278f9992dbf19
saraivais <isadorasaraiva95@gmail.com>
2022-10-25 21:40:44 -0300

    feat [services]: adds function to recover user info~

--- app/backend/src/services/user.service.ts
+++ app/backend/src/services/user.service.ts
@@ -4,6 +4,11 @@ import User from '../database/models/user';
 class UserService {
   constructor(private model: typeof User) {}
 
+  private async recoverUserInformation(email: string):Promise<IUser | null> {
+    const user = await this.model.findOne({ where: { email } });
+    return user;
+  }
+
 }
 
 export default UserService;
