996aa3ea7174e90b5acc3eca3e4b4b04a01ebefa
saraivais <isadorasaraiva95@gmail.com>
2022-10-25 21:47:14 -0300

    feat [services]: adds login function, returning auth token~

--- app/backend/src/services/user.service.ts
+++ app/backend/src/services/user.service.ts
@@ -1,5 +1,9 @@
 import IUser from '../interfaces/IUser';
 import User from '../database/models/user';
+import CustomError from '../errors/customError';
+import errorCatalog from '../errors/errorCatalog';
+import { verifyPassword, hashPassword } from '../helpers/bcrypt';
+import { generateJwtToken } from '../helpers/jwtToken';
 
 class UserService {
   constructor(private model: typeof User) {}
@@ -7,6 +11,17 @@ class UserService {
   private async recoverUserInformation(email: string):Promise<IUser | null> {
     const user = await this.model.findOne({ where: { email } });
     return user;
+  }
+
+  public async login(loginInformation: IUser):Promise<string> {
+    const userInfo = await this.recoverUserInformation(loginInformation.email);
+    if (!userInfo) {
+      throw new CustomError(errorCatalog.userNotFound);
+    }
+    verifyPassword(loginInformation.password, userInfo.password);
+    const token = generateJwtToken(userInfo);
+
+    return token;
   }
 
 }
