0434528fda9e52030e5fb3e7e98b87c1a71392a2
saraivais <isadorasaraiva95@gmail.com>
2022-10-25 21:38:49 -0300

    feat [helpers]: adds function to verify password crypto~

--- app/backend/src/helpers/bcrypt.ts
+++ app/backend/src/helpers/bcrypt.ts
@@ -1,1 +1,10 @@
+import * as bcrypt from 'bcryptjs';
+import CustomError from '../errors/customError';
+import errorCatalog from '../errors/errorCatalog';
 
+export function verifyPassword(password: string, hashedPassword: string):void {
+  const isValid = bcrypt.compareSync(password, hashedPassword);
+  if (!isValid) {
+    throw new CustomError(errorCatalog.wrongPassword);
+  }
+}
