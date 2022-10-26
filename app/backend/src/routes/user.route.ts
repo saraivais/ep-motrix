827161990a9636393273f9f0f0ae569c1b620735
saraivais <isadorasaraiva95@gmail.com>
2022-10-25 21:49:38 -0300

    feat [routes]: creates user router~

--- app/backend/src/routes/user.route.ts
+++ app/backend/src/routes/user.route.ts
@@ -1,1 +1,13 @@
-// rotas para login e criar um usuário
+import express from 'express';
+import UserController from '../controllers/user.controller';
+import UserService from '../services/user.service';
+import User from '../database/models/user';
+import 'express-async-errors';
+
+const userRoute = express.Router();
+
+const userService = new UserService(User);
+const userController = new UserController(userService);
+
+
+export default userRoute;
