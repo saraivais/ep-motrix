174df6185a190b37c2f3b0fa673eabe42b064751
saraivais <isadorasaraiva95@gmail.com>
2022-10-25 21:37:50 -0300

    feat [errors]: adds error middlware w custom error~

--- app/backend/src/errors/errorMiddleware.ts
+++ app/backend/src/errors/errorMiddleware.ts
@@ -1,1 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+import CustomError from './customError';
 
+const errorMiddleware = (
+  error: Error,
+  _request: Request,
+  response: Response,
+  _next: NextFunction,
+):Response => {
+  if (error instanceof CustomError) {
+    const { code, message } = error;
+    return response.status(code).json({ message });
+  }
+
+  return response.status(500).json({ message: 'Server error!' });
+};
+
+export default errorMiddleware;
