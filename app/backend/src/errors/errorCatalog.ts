3014e67feed4fa27ab3fa9f986bee81aa326e7b8
saraivais <isadorasaraiva95@gmail.com>
2022-10-25 21:37:08 -0300

    feat [errorCatalog]: creates some error types~

--- app/backend/src/errors/errorCatalog.ts
+++ app/backend/src/errors/errorCatalog.ts
@@ -1,1 +1,24 @@
+const errorCatalog = {
+  userNotFound: {
+    msg: 'User not found',
+    code: 404,
+    alias: 'Not found',
+  },
+  alreadyInUse: {
+    msg: 'User is already registered',
+    code: 400,
+    alias: 'Bad request',
+  },
+  wrongPassword: {
+    msg: 'Incorrect user/password',
+    code: 401,
+    alias: 'Unauthorized',
+  },
+  invalidToken: {
+    msg: 'Token must be a valid token',
+    code: 401,
+    alias: 'Unauthorized',
+  },
+};
 
+export default errorCatalog;
