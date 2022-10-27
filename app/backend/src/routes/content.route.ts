26dfb60add88d1ce4bb57f5c50024710d40ec5f2
saraivais <isadorasaraiva95@gmail.com>
2022-10-26 22:04:57 -0300

    feat [routes]: creates content route w express router~

--- app/backend/src/routes/content.route.ts
+++ app/backend/src/routes/content.route.ts
@@ -1,1 +1,11 @@
+import express from 'express';
+import ContentController from '../controllers/content.controller';
+import ContentService from '../services/content.service';
+import Content from '../database/models/content';
+import History from '../database/models/history';
+const contentRoute = express.Router();
 
+const contentService = new ContentService(Content, History);
+const contentController = new ContentController(contentService);
+
+export default contentRoute;
