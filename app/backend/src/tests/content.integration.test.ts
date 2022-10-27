ec2bd6e260fdb91854ee8a4cc194885510a331fc
saraivais <isadorasaraiva95@gmail.com>
2022-10-27 17:12:05 -0300

    test [content]: defines test structure w describes~

--- app/backend/src/tests/content.integration.test.ts
+++ app/backend/src/tests/content.integration.test.ts
@@ -1,1 +1,24 @@
+import chai from 'chai';
+import * as sinon from 'sinon';
+import chaiHttp from 'chai-http';
 
+import app from '../app';
+
+import Content from '../database/models/content';
+import History from '../database/models/history';
+
+// mocks
+
+describe('Tests for Content & History', () => {
+  describe('GET /content - Returns all content', () => {});
+
+  describe('GET /content/:id - Returns one content with history & user information', () => {});
+
+  describe('POST /content - Creates a new content and updates history', () => {});
+
+  describe('PATCH /content/:id - Updates a content and updates history', () => {});
+
+  describe('DELETE /content/:id - Deletes one content', () => {});
+
+  describe('DELETE /content - Deletes many contents', () => {});
+});
