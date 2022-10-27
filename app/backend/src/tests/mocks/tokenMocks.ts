3d67ba9c489ad1544ffe98eb865edfb37200dffc
saraivais <isadorasaraiva95@gmail.com>
2022-10-27 17:02:35 -0300

    feat [mocks]: adds jwt token mocks~

---
+++ app/backend/src/tests/mocks/tokenMocks.ts
@@ -0,0 +1,6 @@
+const tokenMocks = {
+  validToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxfSwiaWF0IjoxNjY2NzQ0MTQyfQ.tfrJStybmUN0H-AFbSpEbdTLG8EzBhld0lBe6FnEEVU',
+  invalidToken: 'certainlynotavalidtoken',
+};
+
+export default tokenMocks;
