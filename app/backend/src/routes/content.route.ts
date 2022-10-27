d507dac46fab1b837fd8461621551579503cb7e7
saraivais <isadorasaraiva95@gmail.com>
2022-10-26 22:05:29 -0300

    feat [routes]: adds express-async-errors for async handling

--- app/backend/src/routes/content.route.ts
+++ app/backend/src/routes/content.route.ts
@@ -3,6 +3,7 @@ import ContentController from '../controllers/content.controller';
 import ContentService from '../services/content.service';
 import Content from '../database/models/content';
 import History from '../database/models/history';
+import 'express-async-errors';
 const contentRoute = express.Router();
 
 const contentService = new ContentService(Content, History);
