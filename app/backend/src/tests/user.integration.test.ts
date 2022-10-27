87d19f8667c17897f2a7c7d97365d07bf8f45158
saraivais <isadorasaraiva95@gmail.com>
2022-10-27 10:19:33 -0300

    test [users]: imports & test block creation~

--- app/backend/src/tests/user.integration.test.ts
+++ app/backend/src/tests/user.integration.test.ts
@@ -1,1 +1,16 @@
+import chai from 'chai';
+import * as sinon from 'sinon';
+import chaiHttp from 'chai-http';
 
+import app from '../app';
+
+import User from '../database/models/user';
+
+// mocks
+import userMocks from './mocks/userGeneralMocks';
+
+chai.use(chaiHttp);
+
+describe('Tests for Users & Authorization', () => {
+
+});
