1060d883bf1382dfb0cd4ce1007c17cca668dedd
saraivais <isadorasaraiva95@gmail.com>
2022-10-26 21:57:25 -0300

    feat [services]: creates content service class~

--- app/backend/src/services/content.service.ts
+++ app/backend/src/services/content.service.ts
@@ -1,1 +1,8 @@
+import History from '../database/models/history';
+import Content from '../database/models/content';
+class ContentService {
+  constructor(private model: typeof Content, private history: typeof History) {}
 
+}
+
+export default ContentService;
