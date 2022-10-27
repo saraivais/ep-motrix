9868defcddd7182170a980843c99f4b28cb720f1
saraivais <isadorasaraiva95@gmail.com>
2022-10-27 10:14:34 -0300

    feat [mocks]: adds mocks for users & authorization~

---
+++ app/backend/src/tests/mocks/userGeneralMocks.ts
@@ -0,0 +1,28 @@
+const userMocks = {
+  validUser: {
+    email: 'admin@admin.com',
+    password: '$2b$10$qEGGtrFFKNYt8HrR86CaTeui5gfNHdwW3a3QDB1nVbI5XXLr9f1.m',
+  },
+  validLogin: {
+    email: 'admin@admin.com',
+    password: 'iamadmin',
+  },
+  invalidPassword: {
+    email: 'admin@admin.com',
+    password: 'isweariamadmin',
+  },
+  unregisteredUser: {
+    email: 'totallynotregistered@email.com',
+    password: 'wellitried',
+  },
+  newUser: {
+    email: 'newvalid@account.com',
+    password: 'newpassword',
+  },
+  alreadyRegistered: {
+    email: 'motrix@one.com',
+    password: 'secondtry',
+  },
+};
+
+export default userMocks;
