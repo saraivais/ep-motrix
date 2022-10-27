e2a73151e4ed755c3567d07c99503318187c0275
saraivais <isadorasaraiva95@gmail.com>
2022-10-26 21:55:55 -0300

    feat [interfaces]: adds optional properties & timestamps~

--- app/backend/src/interfaces/IContent.ts
+++ app/backend/src/interfaces/IContent.ts
@@ -1,8 +1,10 @@
 interface IContent {
-  id: number,
-  userId: number,
+  id?: number,
+  userId?: number,
   title: string,
   body: string,
+  created?: Date,
+  updated?: Date,
   user?: {
     email: string,
   },
