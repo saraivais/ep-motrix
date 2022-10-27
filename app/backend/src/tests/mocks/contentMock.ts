bf952f147a7325f6b7225b12493198b78aa96ab8
saraivais <isadorasaraiva95@gmail.com>
2022-10-27 20:08:01 -0300

    fix [mocks]: forgotten userId~

--- app/backend/src/tests/mocks/contentMock.ts
+++ app/backend/src/tests/mocks/contentMock.ts
@@ -62,6 +62,7 @@ const contentMocks = {
   },
   createdContent: {
     id: 12,
+    userId: 2,
     title: 'Duis ultrices justo at justo',
     body: 'Vivamus suscipit neque in ullamcorper condimentum. Morbi et lacus eu augue sollicitudin porta a sit amet mi. Donec varius nec nulla et blandit. Aenean non consectetur magna. Cras eu dui ut massa euismod pharetra sed ac risus. Vivamus consectetur, lorem in commodo feugiat, sem lacus molestie enim, id consectetur arcu purus sed ipsum. Nullam maximus at lectus a dapibus. Quisque eros arcu, mollis non blandit nec, tristique id augue. In ac egestas arcu, tristique varius mi. Nunc commodo sapien a odio tempus consequat.',
     created: '2022-10-21T00:00:00.000Z',
