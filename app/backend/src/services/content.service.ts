49755275dbe973bba5d1c84dc015ef8102cd6bb0
saraivais <isadorasaraiva95@gmail.com>
2022-10-26 21:57:54 -0300

    feat [services]: creates get all function~

--- app/backend/src/services/content.service.ts
+++ app/backend/src/services/content.service.ts
@@ -3,6 +3,10 @@ import Content from '../database/models/content';
 class ContentService {
   constructor(private model: typeof Content, private history: typeof History) {}
 
+  public async getAll():Promise<IContent[]> {
+    const result = await this.model.findAll();
+    return result;
+  }
 }
 
 export default ContentService;
