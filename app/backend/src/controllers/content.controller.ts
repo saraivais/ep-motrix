06d8d6c4ee6b847550c84b09cd063b9fcd004e2d
saraivais <isadorasaraiva95@gmail.com>
2022-10-26 22:01:58 -0300

    feat [controllers]: creates content controller class~

--- app/backend/src/controllers/content.controller.ts
+++ app/backend/src/controllers/content.controller.ts
@@ -1,1 +1,9 @@
+import { Request, Response, NextFunction } from 'express';
+import ContentService from '../services/content.service';
+class ContentController {
+  constructor(private service: ContentService) {}
 
+
+}
+
+export default ContentController;
