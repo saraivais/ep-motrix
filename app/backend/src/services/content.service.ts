ca410905d2f986c0a1c024a6b3d92984ede72940
saraivais <isadorasaraiva95@gmail.com>
2022-10-26 21:58:25 -0300

    feat [services]: creates get by id function, w associations~

--- app/backend/src/services/content.service.ts
+++ app/backend/src/services/content.service.ts
@@ -7,6 +7,33 @@ class ContentService {
     const result = await this.model.findAll();
     return result;
   }
+
+  public async getById(id: number):Promise<IContent> {
+    const content = await this.model.findByPk(id, {
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: {
+            exclude: ['id', 'password'],
+          },
+        }, {
+          model: History,
+          as: 'history',
+          attributes: {
+            exclude: ['id', 'contentId', 'created'],
+          },
+          separate: true,
+          order: [['updated', 'desc']],
+        },
+      ],
+    });
+
+    if (!content) {
+      throw new CustomError(errorCatalog.contentNotFound);
+    }
+    return content;
+  }
 }
 
 export default ContentService;
