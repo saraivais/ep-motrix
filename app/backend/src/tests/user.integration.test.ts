7ab376a10e91b4196f0c91abf1ac0b58ba63778a
saraivais <isadorasaraiva95@gmail.com>
2022-10-27 16:39:42 -0300

    test [users]: user creation~

--- app/backend/src/tests/user.integration.test.ts
+++ app/backend/src/tests/user.integration.test.ts
@@ -27,7 +27,7 @@ describe('Tests for Users & Authorization', () => {
       sinon.restore();
     });
 
-    it('Returns a token when login is successfull', async () => {
+    it('Returns a token when login is successful', async () => {
       const response = await chai.request(app).post('/users/login').send(userMocks.validLogin);
       const { body } = response;
       const { status } = response;
@@ -53,6 +53,40 @@ describe('Tests for Users & Authorization', () => {
 
       chai.expect(status).to.be.equal(401);
       chai.expect(message).to.be.equal('Incorrect user/password');
+    });
+  });
+
+  describe('POST /users - Allows the user to create a new account', () => {
+    before(() => {
+      sinon.stub(User, 'findOne')
+        .onCall(0)
+        .resolves(null)
+        .onCall(1)
+        .resolves(userMocks.alreadyRegistered as User);
+
+      sinon.stub(User, 'create').resolves(userMocks.newUser as User);
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+
+    it('Returns a token when user creation is successful', async () => {
+      const response = await chai.request(app).post('/users').send(userMocks.unregisteredUser);
+      const { body } = response;
+      const { status } = response;
+
+      chai.expect(status).to.be.equal(200);
+      chai.expect(body).to.have.property('token');
+    });
+
+    it('Returns an error message if the email is already registered', async () => {
+      const response = await chai.request(app).post('/users').send({ email: 'motrix@one.com', password: 'secondtry' });
+      const { body: { message } } = response;
+      const { status } = response;
+
+      chai.expect(status).to.be.equal(400);
+      chai.expect(message).to.be.equal('User is already registered');
     });
   });
 });
