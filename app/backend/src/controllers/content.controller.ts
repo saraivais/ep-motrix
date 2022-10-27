37427ae9a75a9105cf3975a92903a68b4f0f5c13
saraivais <isadorasaraiva95@gmail.com>
2022-10-26 22:02:52 -0300

    feat [controllers]: adds get all middleware~

--- app/backend/src/controllers/content.controller.ts
+++ app/backend/src/controllers/content.controller.ts
@@ -3,6 +3,15 @@ import ContentService from '../services/content.service';
 class ContentController {
   constructor(private service: ContentService) {}
 
+  public async getAll(
+    request: Request,
+    response: Response,
+    _next: NextFunction,
+  ): Promise<Response> {
+    const result = await this.service.getAll();
+
+    return response.status(200).json(result);
+  }
 
 }
 
