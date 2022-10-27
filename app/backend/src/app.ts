43e2967dc6b22a00335a817c12878be4cb83b9c4
saraivais <isadorasaraiva95@gmail.com>
2022-10-26 22:07:24 -0300

    feat [app]: imports & uses content router~

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import contentRoute from './routes/content.route';
 import userRoute from './routes/user.route';
 import errorMiddleware from './errors/errorMiddleware';
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/content', contentRoute);
 app.use('/users', userRoute);
 
 app.use(errorMiddleware);
