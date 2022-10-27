954c3152ea7c6facecfdef642e9d0985be759b89
saraivais <isadorasaraiva95@gmail.com>
2022-10-27 18:18:22 -0300

    test [content]: get all route~

--- app/backend/src/tests/content.integration.test.ts
+++ app/backend/src/tests/content.integration.test.ts
@@ -8,9 +8,31 @@ import Content from '../database/models/content';
 import History from '../database/models/history';
 
 // mocks
+import contentMocks from './mocks/contentMock';
+
+chai.use(chaiHttp);
 
 describe('Tests for Content & History', () => {
-  describe('GET /content - Returns all content', () => {});
+  describe('GET /content - Returns all content', () => {
+    before(() => {
+      sinon.stub(Content, 'findAll')
+        .resolves(contentMocks.allContent as unknown as Content[]);
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+
+    it('Returns an array containing all content', async () => {
+      const response = await chai.request(app).get('/content');
+      const { status } = response;
+      const { body } = response;
+
+      chai.expect(status).to.be.equal(200);
+      chai.expect(body).to.be.an('array');
+    });
+  });
+
 
   describe('GET /content/:id - Returns one content with history & user information', () => {});
 
