7abf8ccd1535f9f7058aba41001cd93b3d2e8300
saraivais <isadorasaraiva95@gmail.com>
2022-10-31 19:43:22 -0300

    feat [createOrUpdate]: adds component for content editing~

---
+++ app/frontend/src/pages/CreateOrUpdate.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState, useContext } from 'react';
+import { useLocation, useParams, useNavigate } from 'react-router-dom';
+import { editOneContent, getOneContent, createContent } from '../requests/content';
+import contentContext from '../context/context';
+
+function CreateOrUpdate() {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const { pathname } = useLocation();
+  const [content, setContent] = useState({ title: '', body: '' });
+
+  const { authorization: { auth }, shouldUpdate: { setNewContent } } = useContext(contentContext);
+
+  const onChange = ({ target }) => {
+    const { name, value } = target;
+    setContent((prevState) => (({ ...prevState, [name]: value })));
+  };
+
+  useEffect(() => {
+    const getExistingPost = async () => {
+      const response = await getOneContent(id);
+      setContent(response);
+    };
+    if (pathname !== '/create') {
+      getExistingPost();
+    }
+  }, [pathname, id]);
+
+  const editOrCreate = async () => {
+    if (pathname !== '/create') {
+      await editOneContent(id, auth, content);
+      alert('Post editado com sucesso!');
+      setNewContent(true);
+      navigate(`/content/${id}`);
+    }
+    if (pathname === '/create') {
+      await createContent(auth, content);
+      alert('Post criado com sucesso!');
+      setNewContent(true);
+      navigate('/home');
+    }
+  };
+
+  return (
+    <div>
+      <input name="title" type="text" value={content.title} onChange={onChange} />
+      <textarea name="body" cols="70" rows="10" value={content.body} onChange={onChange} />
+      <button type="button" onClick={editOrCreate}>Enviar</button>
+    </div>
+  );
+}
+
+export default CreateOrUpdate;
