532b9ee9c80dcd5dcadf4136f70aad83a370093b
saraivais <isadorasaraiva95@gmail.com>
2022-10-31 19:30:20 -0300

    feat [app]: adds routes & components~

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Login from './pages/Login';
+import LoginOrCreate from './pages/LoginOrCreate';
 import Content from './pages/Content';
+import Details from './pages/Details';
+import CreateOrUpdate from './pages/CreateOrUpdate';
+import Header from './components/Header';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Content />} />
-      {/* <Route path="" element={} /> */}
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LoginOrCreate />} />
+        <Route path="/signup" element={<LoginOrCreate />} />
+        <Route path="/home" element={<Content />} />
+        <Route path="/content/:id" element={<Details />} />
+        <Route path="/create" element={<CreateOrUpdate />} />
+        <Route path="/update/:id" element={<CreateOrUpdate />} />
+      </Routes>
+    </>
   );
 }
 
