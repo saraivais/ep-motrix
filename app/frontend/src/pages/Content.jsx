51e6379e90c61815d4b380e5f09a37cdd8bfb862
saraivais <isadorasaraiva95@gmail.com>
2022-10-31 19:33:51 -0300

    feat [content]: adds mainpage, displays all contents w cards & adds mass delete option

--- app/frontend/src/pages/Content.jsx
+++ app/frontend/src/pages/Content.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import context from '../context/context';
+import Loading from '../components/Loading';
+import ContentCard from '../components/ContentCard';
+import { massDelete } from '../requests/content';
 
 function Content() {
+  const [display, setDisplay] = useState([]);
+
+  const {
+    content: {
+      allContent,
+    },
+    massDelete: {
+      deleteList,
+      setDeleteList,
+    },
+    authorization: {
+      auth,
+    },
+    shouldUpdate: {
+      setNewContent,
+    },
+  } = useContext(context);
+
+  useEffect(() => {
+    setDisplay(allContent);
+  }, [allContent]);
+
+  const deleteAllAndResetList = async () => {
+    await massDelete(auth, deleteList);
+    setDeleteList([]);
+    setNewContent(true);
+  };
+
   return (
-    <div>
-      Content
+    <div className="content-container">
+      <button type="button" onClick={deleteAllAndResetList}>Deletar todos os selecionados!</button>
+      { display.length === 0 && <Loading />}
+      { display.length !== 0 && display
+        .map((content) => <ContentCard key={content.id} content={content} />)}
     </div>
   );
 }
