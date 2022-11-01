2d6ef728e652c0ac7653ad09622c582736c2d792
saraivais <isadorasaraiva95@gmail.com>
2022-10-31 22:20:08 -0300

    feat [footer]: adds social media links & classnames~

--- app/frontend/src/components/Footer.jsx
+++ app/frontend/src/components/Footer.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import linkedin from '../icons/linkedin-svgrepo-com.svg';
+import github from '../icons/github-svgrepo-com.svg';
+import '../style/Footer.css';
 
 function Footer() {
   return (
-    <footer>
-      Desenvolvido por Isadora Saraiva, Outubro 2022.
+    <footer className="footer-container">
+      <p className="footer-text">Desenvolvido por Isadora Saraiva - Outubro/2022</p>
+      <div>
+        <a href="https://www.linkedin.com/in/isadorasaraiva/" target="_blank" rel="noreferrer">
+          <img className="footer-link-images" src={linkedin} alt="linkedin-icon" />
+        </a>
+        <a href="https://github.com/saraivais" target="_blank" rel="noreferrer">
+          <img className="footer-link-images" src={github} alt="github-icon" />
+        </a>
+      </div>
     </footer>
   );
 }
